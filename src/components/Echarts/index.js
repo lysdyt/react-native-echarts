cb925851b9aee3b64bddfc0234dd94ffe64f0454
oulp <289366465@qq.com>
2018-02-08 10:39:17 +0800

    解决数据刷新闪烁、安卓打包不显示

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,31 +1,64 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet } from 'react-native';
+import { WebView, View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
+import renderChartNoFirst from './renderChart';
 import echarts from './echarts.min';
 
 export default class App extends Component {
+  // 预防过渡渲染
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const thisProps = this.props || {};
+    nextProps = nextProps || {};
+    if (Object.keys(thisProps).length !== Object.keys(nextProps).length) {
+      return true;
+    }
+    for (const key in nextProps) {
+      if (JSON.stringify(thisProps[key]) != JSON.stringify(nextProps[key])) {
+        // console.log('props', key, thisProps[key], nextProps[key])
+        return true;
+      }
+    }
+    return false;
+  }
+
   componentWillReceiveProps(nextProps) {
-    if(nextProps.option !== this.props.option) {
-      this.refs.chart.reload();
+    if (nextProps.option !== this.props.option) {
+      // 解决数据改变时页面闪烁的问题
+      this.refs.chart.injectJavaScript(renderChart(nextProps, false));
     }
   }
 
   render() {
-    return (
-      <View style={{flex: 1, height: this.props.height || 400,}}>
-        <WebView
-          ref="chart"
-          scrollEnabled = {false}
-          injectedJavaScript = {renderChart(this.props)}
-          style={{
-            height: this.props.height || 400,
-            backgroundColor: this.props.backgroundColor || 'transparent'
-          }}
-          scalesPageToFit={false}          
-          source={require('./tpl.html')}
-          onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
-        />
-      </View>
-    );
+    if (Platform.OS == 'android') {
+      return (
+        <View style={{ flex: 1, height: this.props.height || 400 }}>
+          <WebView
+            ref="chart"
+            scrollEnabled={false}
+            injectedJavaScript={renderChart(this.props, true)}
+            style={{
+              height: this.props.height || 400
+            }}
+            source={{ uri: 'file:///android_asset/echarts/tpl.html' }}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={{ flex: 1, height: this.props.height || 400 }}>
+          <WebView
+            ref="chart"
+            scrollEnabled={false}
+            scalesPageToFit={false}
+            injectedJavaScript={renderChart(this.props, true)}
+            style={{
+              height: this.props.height || 400
+            }}
+            source={require('./tpl.html')}
+          />
+        </View>
+      );
+    }
   }
 }
